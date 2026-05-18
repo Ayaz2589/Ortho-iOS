7924d7848e7b8020afe88e3f5d1bfa6e504691fd
Ayaz Uddin <ayaz@crossterra.com>
2026-05-18 09:10:32 -0400

    Initial Commit

---
+++ Ortho-iOS/Ortho_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ortho_iOSApp.swift
+//  Ortho-iOS
+//
+//  Created by Ayaz Uddin on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Ortho_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
